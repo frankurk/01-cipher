7c79131f0d915f80f111eef2d1ce9fcf9371612f
Carolina <carolinazapatavidal@gmail.com>
2022-03-24 12:43:46 -0600

    encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,11 +1,12 @@
 const cipher = {
   // ...
-  const alphabet = ["A","B","C","D","E","F","G","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  const nickname = document.getElementById('nickname');
-  const range = document.getElementById('range');
+//   const alphabet = ["A","B","C","D","E","F","G","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+//   const nickname = document.getElementById('nickname');
+//   const range = document.getElementById('range');
 
-  const element = document.getElementById("start");
-element.addEventListener("click", function()
+//   const element = document.getElementById("start");
+// element.addEventListener("click", function()
+encode(){}
 };
 
 export default cipher;
