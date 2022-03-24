0164d0026c1bda002b8317209cd028c2e8bd0ad3
Carolina <carolinazapatavidal@gmail.com>
2022-03-24 11:10:58 -0600

    act javas

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,11 @@
 const cipher = {
   // ...
+  const alphabet = ["A","B","C","D","E","F","G","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  const nickname = document.getElementById('nickname');
+  const range = document.getElementById('range');
+
+  const element = document.getElementById("start");
+element.addEventListener("click", function()
 };
 
 export default cipher;
