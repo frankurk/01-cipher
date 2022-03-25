ae8192cd345117d6609eac122e6c6a2bceac000f
Carolina <carolinazapatavidal@gmail.com>
2022-03-25 11:48:59 -0600

    try4

--- src/cipher.js
+++ src/cipher.js
@@ -1,21 +1,19 @@
 const letters = [a,b,c,d,f,g,h,i,j,k,l,m,'ñ',o,p,q,r,s,t,u,v,w,x,y,z]
 const ñ = ['\u00F1']
+const newLetters = {};
+const adjustedKey = {key % 26};
+const letterOfString = 'letters'
 const cipher = {
   // ...
-  const newLetters = {};
-  const adjustedKey = {key % 26};
-
-  for (const letter of string) {
+   for (const letterOfString) {
      let newLetter = (letter.charCodeAt() + adjustedKey)
      if (newLetter <= 122) {
         newLetters.push(String.fromCharCode(newLetter));
      } else if (newLetter > 122) {
         newLetters.push(String.fromCharCode(96 + (newLetter % 122)));
+     return (newLetters.join(''))
      }
-  }
-  return (newLetters.join(''))
-}
+    }
 encode(){}
 };
-
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import cipher from './cipher.js';
 
 const nickname = document.getElementById('nickname');
 const range = document.getElementById('range');
-const ñ = '\u00F1';
 
-
-const element = document.getElementById("start");
+const encode = document.getElementById("encode");
 element.addEventListener("click", function()
 
+const decode = document.getElementById("decode");
+element.addEventListener("click",
+    function newDoc() 
+    {window.location.assign("http://www.src.cl/decode.html")
 console.log(cipher);
